585a5f3d532f551d4e94f840643c2bb884447abd
Nathan Villicana-Shaw <nathan@Nathans-MacBook-Pro.local>
2020-09-21 11:32:50 -0700

    updated value tracker float

--- ValueTrackerFloat.h
+++ ValueTrackerFloat.h
@@ -9,39 +9,53 @@ class ValueTrackerFloat() {
       void update();
 
       float getPosDelta();
-      float getMin(){return min_recorded;};
-      float getMax(){return max_recorded;};
-      float getAvg(){return average_val;};
 
-      float getScaledValue();
+      float getMin(bool reset);
+      float getMin(){getMin(false);};
+
+      float getMax(bool reset);
+      float getMax(){getMax(false);};
+
+      float getAvg(bool reset);
+      float getAvg(){getAvg(false)};
+
+      float getRAvg(bool reset);
+      float getRAvg(){getRAvg(false)};
+
+      float getScaled();
 
   private:
-    float min_recorded =  99999.9;
-    float max_recorded = -99999.9;
+    float min_recorded =                   99999.9;
+    float max_recorded =                   -99999.9;
 
-    float average_val = 0.0;
-    uint64_t num_values = 0;  // how many values are in the "average_val" calculator
+    /////////// average
+    float ravg_val =                      0.0;
+    float totals =                         0.0;
 
-    float pos_delta = 0.0;
+    // how many values are in the "average_val" calculator
+    uint64_t num_values =                   0;
+    float pos_delta =                      0.0;
+
     float &val;
-    float _last_val;
+    float last_val;
 
     // how much will the new value effect the min/max values?
     // should be set to 1.0 under most circumstances
-    float factor = 1.0;
+    float ravg_factor =                         1.0;
 }
 
-ValueTrackerFloat(float &val) {
+ValueTrackerFloat(double &val) {
     last_val = _val;
     val = _val;
     min_recorded = _val;
     max_recorded = _val;
-    average_val = _val;
+    ravg_val = _val;
+    totals = _val;
     num_values++;
 };
 
-float getScaledValue() {
- return (val - min_recorded) / max_recorded; 
+double getScaled() {
+ return (val - min_recorded) / max_recorded;
 }
 
 void ValueTrackerFloat::update() {
@@ -55,7 +69,7 @@ void ValueTrackerFloat::update() {
     }
 
     // min_value update /////////////////////////////////
-    if (val > min_recorded) {
+    if (val < min_recorded) {
         if (factor != 1.0) {
             min_recorded = (min_recorded * (1.0 - factor)) + (val * factor);
         } else {
@@ -66,23 +80,39 @@ void ValueTrackerFloat::update() {
     // take the running average and multiple it against now many readings there have
     // been thus far, then add current value and divide by total number of readings
     // including current reading.
-    average_val = ((average_val * num_values) + val) / (num_values + 1);
-    num_values++;
+    ravg_val = (last_val + val) * ravg_factor;
+
+    total += val;
+
+    if (average_active || rolling_average_active == true) {
+        num_values++;
+    }
 
     // pos delta update
-    delta = val - last_val;
+    if (pos_delta_active == true) {
+        delta = val - last_val;
+    }
     last_val = val;
 }
 
-float ValueTrackerFloat::getPosDelta() {
+double ValueTrackerFloat::getAverage() {
+    return total / num_values;
+}
+
+double ValueTrackerFloat::resetAverage() {
+    total = 0;
+
+}
+
+double ValueTrackerFloat::getPosDelta() {
     if (delta > 0.0) {
         return delta;
     }
     return 0.0;
 }
 
-void ValueTrackerFloat::setUpdateFactor(float _factor) {
+void ValueTrackerFloat::setUpdateFactor(double _factor) {
     factor = _factor;
 }
 
-#endif // __VALUE_TRACKER_FLOAT_H__
+#endif // __VALUE_TRACKER_DOUBLE_H__
